9b25000cf4182dc63719f9fbd80da6bb677718ff
Dev_Lee <toxic023@naver.com>
2023-11-11 20:07:48 +0900

    first commit

---
+++ src/main/kotlin/com/example/luckyfind/LuckyFindApplication.kt
@@ -0,0 +1,11 @@
+package com.example.luckyfind
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LuckyFindApplication
+
+fun main(args: Array<String>) {
+    runApplication<LuckyFindApplication>(*args)
+}
