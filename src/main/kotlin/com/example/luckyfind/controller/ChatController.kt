d7f1ef318dc8909aba9c2fdb0a59944ab8934799
Dev_Lee <toxic023@naver.com>
2023-11-25 20:59:44 +0900

    controller modify
    채팅 컨트롤러와 메시지 컨트롤러 구분

--- src/main/kotlin/com/example/luckyfind/controller/ChatController.kt
+++ src/main/kotlin/com/example/luckyfind/controller/ChatController.kt
@@ -12,47 +12,24 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "Chat API", description = "Chat 서비스 API Test")
 @RestController
+@RequestMapping("/api/v1")
 class ChatController(
     private val chatService: ChatService,
 ) {
 
-    // 메시지 전송 매핑
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    fun sendMessage(
-        @Payload chatMessage: ChatMessage?
-    ): ChatMessage? {
-        return chatMessage
-    }
-
-    //채팅참가자 등록
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    fun addUser(@Payload chatMessage: ChatMessage, headerAccessor: SimpMessageHeaderAccessor): ChatMessage? {
-        headerAccessor.sessionAttributes!!["username"] = chatMessage.sender
-        return chatMessage
-    }
-
-    //채팅참가자 퇴장
-    @MessageMapping("/chat.leaveUser")
-    @SendTo("/topic/public")
-    fun leaveUser(@Payload chatMessage: ChatMessage, headerAccessor: SimpMessageHeaderAccessor): ChatMessage? {
-        headerAccessor.sessionAttributes!!["username"] = chatMessage.sender
-        return chatMessage
-    }
-
-
     // 채팅생성
-    @PostMapping("/api/v1/chat")
+    @PostMapping("/chat")
     fun createChat(
         @RequestBody request: ChatRequest,
     ) = chatService.createChat(request)
 
-    @GetMapping("/api/v1/chat/all")
+    // 모든 채팅방 호출
+    @GetMapping("/chat/all")
     @Operation(summary = "모든 채팅방을 호출합니다.")
     fun getChatList() = chatService.getChatList()
 }
---
+++ src/main/kotlin/com/example/luckyfind/controller/MessageController.kt
@@ -0,0 +1,37 @@
+package com.example.luckyfind.controller
+
+import com.example.luckyfind.model.ChatMessage
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MessageController {
+
+    // 메시지 전송 매핑
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    fun sendMessage(
+        @Payload chatMessage: ChatMessage?
+    ): ChatMessage? {
+        return chatMessage
+    }
+
+    //채팅참가자 등록
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    fun addUser(@Payload chatMessage: ChatMessage, headerAccessor: SimpMessageHeaderAccessor): ChatMessage? {
+        headerAccessor.sessionAttributes!!["username"] = chatMessage.sender
+        return chatMessage
+    }
+
+    //채팅참가자 퇴장
+    @MessageMapping("/chat.leaveUser")
+    @SendTo("/topic/public")
+    fun leaveUser(@Payload chatMessage: ChatMessage, headerAccessor: SimpMessageHeaderAccessor): ChatMessage? {
+        headerAccessor.sessionAttributes!!["username"] = chatMessage.sender
+        return chatMessage
+    }
+}
